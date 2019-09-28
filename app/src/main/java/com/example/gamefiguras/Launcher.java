08ec3f733e7655a12feb5654ee980659f30e123c
CodeDanielMartinez <danielmartinezdm@hotmail.com>
2019-09-28 11:17:48 -0500

    Primer Commit

--- app/src/main/java/com/example/gamefiguras/Launcher.java
+++ app/src/main/java/com/example/gamefiguras/Launcher.java
@@ -2,13 +2,28 @@ package com.example.gamefiguras;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Launcher extends AppCompatActivity {
+
+    private final int DURACION_SPLASH = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(Launcher.this,MainActivity.class);
+                Launcher.this.startActivity(mainIntent);
+                Launcher.this.finish();
+
+            }
+        },
+     DURACION_SPLASH);
     }
 }
--- app/src/main/java/com/example/gamefiguras/MainActivity.java
+++ app/src/main/java/com/example/gamefiguras/MainActivity.java
@@ -33,8 +33,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
